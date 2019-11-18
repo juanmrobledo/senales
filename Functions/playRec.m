9c44a0a94930d8b052d5a381777ff9280cc2c0e0
juanmrobledo <robledojuanm@gmail.com>
2019-11-18 14:30:48 -0300

    NO TOCAR PLAYREC

--- Functions/playRec.m
+++ Functions/playRec.m
@@ -17,21 +17,14 @@ function playRec(f1,f2,Time,Device)
     fileReader = dsp.AudioFileReader('Sine Sweep.wav');
     fileWriter = dsp.AudioFileWriter('Recorded Signal.wav','SampleRate',Fs);
     devicePlayRec = audioPlayerRecorder('Device',Device,'SampleRate',Fs);
-<<<<<<< HEAD
-=======
-   
->>>>>>> 8f92122552ab8964fddb0c876e02fe473bfc30c5
 
 %% Bucle de Adquisicion
 disp('Inicio de Adquisición')
 while ~isDone(fileReader)  
     
     audioToPlay = fileReader();
-<<<<<<< HEAD
     [audioRecorded,nUnderruns,nOverruns] = devicePlayRec(audioToPlay);
-=======
     [audioRecorded,nOverruns] = devicePlayRec(audioToPlay);
->>>>>>> 8f92122552ab8964fddb0c876e02fe473bfc30c5
     fileWriter(audioRecorded)
     
     if nUnderruns > 0
@@ -42,14 +35,10 @@ while ~isDone(fileReader)
     end
 end
 
-    disp('Adquisición Finalizada')
+   % disp('Adquisición Finalizada')
 
 release(fileReader)
 release(fileWriter)
 release(devicePlayRec)
-<<<<<<< HEAD
-end
 
-=======
 end
->>>>>>> 8f92122552ab8964fddb0c876e02fe473bfc30c5
